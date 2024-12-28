848005e39105cdc6c176a2638243f75788b87d49
andresgrondona <afgo4ster@gmail.com>
2024-12-28 15:53:39 -0500

    mongo URI

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,38 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("MongoDB connected successfully");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error.message);
+//     //process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect('mongodb+srv://afgo4ster:fjcbBQLEPUiXXnIW@cluster0.bw5hk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message);
-    //process.exit(1);
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Failed to connect to MongoDB', err);
+    process.exit(1); // Detener la aplicación si no se puede conectar
   }
 };
 
 module.exports = connectDB;
+
+
+
+
+
